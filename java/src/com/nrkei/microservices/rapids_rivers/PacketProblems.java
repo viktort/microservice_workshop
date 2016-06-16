c9a0ed214afbc470afbdcc96cbb9f6c53246d7fb
Fred George <fredgeorge@acm.org>
2016-06-16 17:44:52 +0200

    Fred: Created PacketProblems to record everything wrong with a JSON packet

--- java/src/com/nrkei/microservices/rapids_rivers/PacketProblems.java
+++ java/src/com/nrkei/microservices/rapids_rivers/PacketProblems.java
@@ -5,16 +5,69 @@ package com.nrkei.microservices.rapids_rivers;
  * @author Fred George
  */
 
+import java.util.ArrayList;
+import java.util.List;
+
 // Understands issue that arose when analyzing a JSON message
 public class PacketProblems {
 
     private final String originalJson;
+    private final List<String> informationMessages = new ArrayList<>();
+    private final List<String> warnings = new ArrayList<>();
+    private final List<String> errors = new ArrayList<>();
+    private final List<String> severeErrors = new ArrayList<>();
 
     public PacketProblems(String originalJson) {
         this.originalJson = originalJson;
     }
 
     public boolean hasErrors() {
-        return false;
+        return !(errors.isEmpty() && severeErrors.isEmpty());
+    }
+
+    private boolean hasMessages() {
+        return !informationMessages.isEmpty() || !warnings.isEmpty() || hasErrors();
+    }
+
+    public void information(String explanation) {
+        informationMessages.add(explanation);
+
+    }
+    public void warning(String explanation) {
+        warnings.add(explanation);
+    }
+
+    public void error(String explanation) {
+        errors.add(explanation);
+    }
+
+    public void severeError(String explanation) {
+        severeErrors.add(explanation);
+    }
+
+    @Override
+    public String toString() {
+        if (!hasMessages()) return "No errors detected in JSON:\n\t" + originalJson;
+        StringBuffer results = new StringBuffer();
+        results.append("Messages exist. Original JSON string is:\n\t");
+        results.append(originalJson);
+        append("Severe errors", severeErrors, results);
+        append("Errors", errors, results);
+        append("Warnings", warnings, results);
+        append("Information", informationMessages, results);
+        results.append("\n");
+        return results.toString();
+    }
+
+    private void append(String label, List<String> messages, StringBuffer results) {
+        if (messages.isEmpty()) return;
+        results.append("\n");
+        results.append(label);
+        results.append(": ");
+        results.append(messages.size());
+        for (String message : messages) {
+            results.append("\n\t");
+            results.append(message);
+        }
     }
 }
--- java/test/com/nrkei/microservices/rapids_rivers/PacketProblemsTest.java
+++ java/test/com/nrkei/microservices/rapids_rivers/PacketProblemsTest.java
@@ -1,5 +1,7 @@
 package com.nrkei.microservices.rapids_rivers;
 
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -13,11 +15,45 @@ import static org.junit.Assert.*;
 // Ensures that PacketProblems operates correctly
 public class PacketProblemsTest {
 
-    private final static String VALID_JSON =
-            "{\"key1\":\"value1\"}";
+    private final static String VALID_JSON = "{\"key1\":\"value1\"}";
+    
+    private PacketProblems problems;
+
+    @Before
+    public void setUp() throws Exception {
+        problems = new PacketProblems(VALID_JSON);
+    }
 
     @Test
     public void noProblemsFoundDefault() throws Exception {
-        assertFalse(new PacketProblems(VALID_JSON).hasErrors());
+        assertFalse(problems.hasErrors());
+    }
+
+    @Test
+    public void errorsDetected() throws Exception {
+        problems.error("Simple error");
+        assertTrue(problems.hasErrors());
+        assertThat(problems.toString(), containsString("Simple error"));
+    }
+
+    @Test
+    public void severeErrorsDetected() throws Exception {
+        problems.severeError("Severe error");
+        assertTrue(problems.hasErrors());
+        assertThat(problems.toString(), containsString("Severe error"));
+    }
+
+    @Test
+    public void warningsDetected() throws Exception {
+        problems.warning("Warning explanation");
+        assertFalse(problems.hasErrors());
+        assertThat(problems.toString(), containsString("Warning explanation"));
+    }
+
+    @Test
+    public void informationalMessagesDetected() throws Exception {
+        problems.warning("Information only message");
+        assertFalse(problems.hasErrors());
+        assertThat(problems.toString(), containsString("Information only message"));
     }
 }
