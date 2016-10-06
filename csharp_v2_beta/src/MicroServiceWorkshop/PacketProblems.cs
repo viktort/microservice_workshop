5d5c40ecb9f8ec6ba8e24031ac3587922e660d55
Fred George <fredgeorge@acm.org>
2016-10-06 10:41:14 +0800

    Fred: Skeleton for C# v2 (source and test directories)

---
+++ csharp_v2_beta/src/MicroServiceWorkshop/PacketProblems.cs
@@ -0,0 +1,13 @@
+﻿/* 
+ * Copyright (c) 2016 by Fred George
+ * May be used freely except for training; license required for training.
+ */
+
+ namespace MicroServiceWorkshop
+{
+    // Understands various information about a received JSON packet
+    public class PacketProblems
+    {
+        
+    }
+}
