18b48582caeb366eebefdc88b7661911c3f910aa
Fred George <fredgeorge@acm.org>
2016-10-07 18:58:53 +0800

    Fred: Packet created with parsing/validation of JSON string

---
+++ csharp_v2_beta/src/MicroServiceWorkshop/RapidsRivers/Packet.cs
@@ -0,0 +1,35 @@
+﻿/* 
+ * Copyright (c) 2016 by Fred George
+ * May be used freely except for training; license required for training.
+ */
+
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MicroServiceWorkshop.RapidsRivers
+{
+    // Understands a well-formed message
+    public class Packet
+    {
+        private readonly JObject _jsonHash;
+        private readonly PacketProblems _problems;
+
+        public Packet(string jsonString, PacketProblems problems)
+        {
+            _problems = problems;
+            try
+            {
+                _jsonHash = JObject.Parse(jsonString);
+            }
+            catch (JsonException e)
+            {
+                _problems.SevereError("Invalid JSON format per NewtonSoft JSON library");
+            }
+            catch (Exception e)
+            {
+                problems.SevereError("Unknown failure. Message is: " + e.Message);
+            }
+        }
+    }
+}
---
+++ csharp_v2_beta/test/MicroServiceWorkshop.Tests/RapidsRivers/PacketTest.cs
@@ -0,0 +1,46 @@
+﻿/* 
+ * Copyright (c) 2016 by Fred George
+ * May be used freely except for training; license required for training.
+ */
+
+using MicroServiceWorkshop.RapidsRivers;
+using NUnit.Framework;
+
+namespace MicroServiceWorkshop.Tests.RapidsRivers
+{
+    // Ensures Packets are well-formed or catch construction errors
+    [TestFixture]
+    public class PacketTest
+    {
+        private static readonly string SolutionString =
+            "{\"need\":\"car_rental_offer\"," +
+            "\"user_id\":456," +
+            "\"solutions\":[" +
+            "{\"offer\":\"15% discount\"}," +
+            "{\"offer\":\"500 extra points\"}," +
+            "{\"offer\":\"free upgrade\"}" +
+            "]," +
+            "\"frequent_renter\":\"\"," +
+            "\"system.read_count\":2," +
+            "\"contributing_services\":[]}";
+
+        private static readonly string MissingComma =
+            "{\"frequent_renter\":\"\" \"read_count\":2}";
+
+        [Test]
+        public void ValidJson()
+        {
+            PacketProblems problems = new PacketProblems(SolutionString);
+            Packet p = new Packet(SolutionString, problems);
+            Assert.False(problems.HasErrors());
+        }
+
+        [Test]
+        public void InvalidJson()
+        {
+            PacketProblems problems = new PacketProblems(MissingComma);
+            Packet p = new Packet(MissingComma, problems);
+            Assert.True(problems.HasErrors());
+        }
+    }
+}
