4dd3bcc47de8ddbcce0c6076695fe641410e18f3
Fred George <fredgeorge@acm.org>
2016-10-08 14:40:16 +0800

    Fred: Packet with Require keys method

--- csharp_v2_beta/src/MicroServiceWorkshop/RapidsRivers/Packet.cs
+++ csharp_v2_beta/src/MicroServiceWorkshop/RapidsRivers/Packet.cs
@@ -4,6 +4,8 @@
  */
 
 using System;
+using System.Collections.Generic;
+using System.Reflection;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -14,6 +16,7 @@ namespace MicroServiceWorkshop.RapidsRivers
     {
         private readonly JObject _jsonHash;
         private readonly PacketProblems _problems;
+        private readonly Dictionary<string, object> _recognized_keys = new Dictionary<string, object>();
 
         public Packet(string jsonString, PacketProblems problems)
         {
@@ -30,6 +33,38 @@ namespace MicroServiceWorkshop.RapidsRivers
             {
                 _problems.SevereError("Unknown failure. Message is: " + e.Message);
             }
+        }
+
+        public void Require(params string[] requiredJsonKeys)
+        {
+            foreach (string key in requiredJsonKeys)
+            {
+                if (HasKey(key)) AddAccessor(key);
+                else _problems.Error("Missing required key '" + key + "'");
+            }
+        }
+
+        private bool HasKey(string key)
+        {
+            return _jsonHash[key].Type != JTokenType.Null;
+        }
+
+        private void AddAccessor(string key)
+        {
+            if (_recognized_keys.ContainsKey(key)) return;
+            if (_jsonHash[key].Type == JTokenType.String)
+                _recognized_keys[key] = (string) _jsonHash[key];
+            else if (_jsonHash[key].Type == JTokenType.Integer)
+                _recognized_keys[key] = (int)_jsonHash[key];
+            else if (_jsonHash[key].Type == JTokenType.Float)
+                _recognized_keys[key] = (float) _jsonHash[key];
+            else
+                _recognized_keys[key] = _jsonHash[key];
+        }
+
+        public object Get(string key)
+        {
+            return _recognized_keys[key];
         }
     }
 }
--- csharp_v2_beta/test/MicroServiceWorkshop.Tests/RapidsRivers/PacketTest.cs
+++ csharp_v2_beta/test/MicroServiceWorkshop.Tests/RapidsRivers/PacketTest.cs
@@ -12,27 +12,44 @@ namespace MicroServiceWorkshop.Tests.RapidsRivers
     [TestFixture]
     public class PacketTest
     {
-        private static readonly string SolutionString =
+        private const string SolutionString =
             "{\"need\":\"car_rental_offer\"," +
             "\"user_id\":456," +
             "\"solutions\":[" +
-            "{\"offer\":\"15% discount\"}," +
-            "{\"offer\":\"500 extra points\"}," +
-            "{\"offer\":\"free upgrade\"}" +
-            "]," +
+                "{\"offer\":\"15% discount\"}," +
+                "{\"offer\":\"500 extra points\"}," +
+                "{\"offer\":\"free upgrade\"}" +
+                "]," +
             "\"frequent_renter\":\"\"," +
+            "\"sample_float_key\":1.25," +
             "\"system.read_count\":2," +
             "\"contributing_services\":[]}";
 
-        private static readonly string MissingComma =
+        private const string MissingComma =
             "{\"frequent_renter\":\"\" \"read_count\":2}";
+
+        private const string NeedKey = "need";
+        private const string UserIdKey = "user_id";
+        private const string SampleFloatKey = "sample_float_key";
+        private const string KeyToBeAdded = "key_to_be_added";
+        private const string EmptyArrayKey = "contributing_services";
+        private const string InterestingKey = "frequent_renter";
+        private const string SolutionsKey = "solutions";
+
+        private PacketProblems _problems;
+        private Packet _packet;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _problems = new PacketProblems(SolutionString);
+            _packet = new Packet(SolutionString, _problems);
+        }
 
         [Test]
         public void ValidJson()
         {
-            PacketProblems problems = new PacketProblems(SolutionString);
-            Packet p = new Packet(SolutionString, problems);
-            Assert.False(problems.HasErrors());
+            Assert.False(_problems.HasErrors());
         }
 
         [Test]
@@ -42,6 +59,40 @@ namespace MicroServiceWorkshop.Tests.RapidsRivers
             Packet p = new Packet(MissingComma, problems);
             Assert.True(problems.HasErrors());
             Assert.That(problems.ToString(), Does.Contain("Invalid JSON"));
+        }
+
+        [Test]
+        public void RequiredStringKey()
+        {
+            _packet.Require(NeedKey);
+            Assert.False(_problems.HasErrors());
+            Assert.AreEqual("car_rental_offer", _packet.Get(NeedKey));
+        }
+
+        [Test]
+        public void RequiredIntegerKey()
+        {
+            _packet.Require(UserIdKey);
+            Assert.False(_problems.HasErrors());
+            Assert.AreEqual(456, _packet.Get(UserIdKey));
+        }
+
+        [Test]
+        public void RequiredFloatKey()
+        {
+            _packet.Require(SampleFloatKey);
+            Assert.False(_problems.HasErrors());
+            Assert.AreEqual(1.25, _packet.Get(SampleFloatKey));
+        }
+
+        [Test]
+        public void MultipleRequiredKeys()
+        {
+            _packet.Require(NeedKey, UserIdKey, SampleFloatKey);
+            Assert.False(_problems.HasErrors());
+            Assert.AreEqual("car_rental_offer", _packet.Get(NeedKey));
+            Assert.AreEqual(456, _packet.Get(UserIdKey));
+            Assert.AreEqual(1.25, _packet.Get(SampleFloatKey));
         }
     }
 }
