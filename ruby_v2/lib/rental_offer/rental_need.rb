3fc4f26acff441f2a5c39b57ffd8c6885e827d2e
Fred George <fredgeorge@acm.org>
2017-03-09 10:55:03 +0000

    Fred: Ruby V2 has working pair of services

---
+++ ruby_v2/lib/rental_offer/rental_need.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+# Copyright (c) 2017 by Fred George.
+# May be used freely except for training; license required for training.
+
+# For debugging...
+require 'pry'
+require 'pry-nav'
+
+require 'securerandom'
+
+require '../rapids_rivers/rabbit_mq/rabbit_mq_rapids'
+
+# Understands the complete stream of messages on an event bus
+class RentalNeed
+  attr_reader :service_name
+
+  def initialize(host_ip, port)
+    @rapids_connection = RabbitMqRapids.new(host_ip, port)
+    @service_name = 'rental_need_ruby_' + SecureRandom.uuid
+  end
+
+  def start
+    loop do
+      @rapids_connection.publish need_packet
+      puts " [x] Published a rental offer need on the bus:\n\t     #{need_packet.to_json}"
+      sleep 5
+    end
+  end
+
+  private
+
+    def need_packet
+      Packet.new(need: 'rental_car_offer')
+    end
+
+end
+
+RentalNeed.new(ARGV.shift, ARGV.shift.to_i).start
